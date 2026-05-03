c4d80e576d4b992050560928a5e17cd95d017d08
Zabedfolio <zabedfolio@gmail.com>
2026-05-03 22:09:20 +0600

    added category wise filter and all photos page design

--- src/app/all-photos/page.jsx
+++ src/app/all-photos/page.jsx
@@ -1,9 +1,30 @@
+import Category from '@/components/Category';
+import PhotoCard from '@/components/PhotoCard';
 import React from 'react';
 
-const AllPhotosPage = () => {
+const AllPhotosPage = async({searchParams}) => {
+    const {category} = await searchParams;
+    const res = await fetch('https://pixgen-ai-zabedfolio.vercel.app/data/data.json');
+    const photos = await res.json();
+    // const topPhotos = photos.slice(0,8);
+    // console.log(photos, "photos")
+    let filteredPhotos;
+    if(category){
+        filteredPhotos = photos.filter(photo => photo.category.toLowerCase()== category.toLowerCase());
+    }else{
+        filteredPhotos= photos;
+    }
+
+    
     return (
-        <div>
-            This is all photos page
+        <div className='container mx-auto'>
+            <h1 className='font-bold text-2xl my-5'>All Photos</h1>
+            <Category></Category>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
+                {
+                    filteredPhotos.map(photo => <PhotoCard key={photo.id} photo={photo}></PhotoCard>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/PhotoCard.jsx
+++ src/components/PhotoCard.jsx
@@ -6,16 +6,16 @@ import { FaHeart } from "react-icons/fa6";
 import Image from 'next/image';
 
 
-const PhotoCard = ({photo}) => {
+const PhotoCard = ({ photo }) => {
     return (
-        <Card className="border rounded-xl">
+        <Card className="border border-[#dfdfdf] rounded-xl">
             <div className="relative w-full aspect-square">
-            <Image
-                src={photo.imageUrl}
-                fill
-                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-                alt={photo.title}
-                className="object-cover rounded-xl"
+                <Image
+                    src={photo.imageUrl}
+                    fill
+                    sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                    alt={photo.title}
+                    className="object-cover rounded-xl"
                 />
 
                 <Chip size="sm" className="absolute right-2 top-2">{photo.category}</Chip>
@@ -25,23 +25,25 @@ const PhotoCard = ({photo}) => {
                 <h2 className="font-medium">{photo.title}</h2>
             </div>
 
-           <div className="flex gap-5">
-             <div className="flex items-center gap-2">
-                <p><FaHeart /></p>
-                <p>{photo.likes}</p>
+            <div className="flex gap-5">
+                <div className="flex items-center gap-2">
+                    <p><FaHeart /></p>
+                    <p>{photo.likes}</p>
+                </div>
+
+                <Separator orientation="vertical" />
+
+
+                <div className="flex items-center gap-2">
+                    <p><BiDownload /></p>
+                    <p>{photo.downloads}</p>
+                </div>
             </div>
 
-            <Separator orientation="vertical"/>
+            <Link href={`/all-photos/${photo.id}`}>      
+            <Button variant="outline" color="#dfdfdf" className={'w-full'}>View</Button>
+            </Link>
 
-
-               <div className="flex items-center gap-2">
-                <p><BiDownload /></p>
-                <p>{photo.downloads}</p>
-            </div>
-           </div>
-
-     <Link href={`/all-photos/${photo.id}`}>      <Button variant="outline" className={'w-full'}>View</Button></Link>
-            
         </Card>
     );
 };
